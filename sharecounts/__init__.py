fcaaf31272f77adf597ebf01d9dee53522271eaf
saml <skynare@gmail.com>
2013-09-16 10:59:24 -0400

    templatifying

--- sharecounts/__init__.py
+++ sharecounts/__init__.py
@@ -1,7 +1,7 @@
 import argparse
 
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config.from_object('sharecounts.settings')
 
@@ -17,11 +17,5 @@ def sharecounts():
     fbcount = fbshares + fbcomments
     twcount = tw.get('count', 0)
     data = {'url': url, 'fbcount': fbcount, 'fbshares': fbshares, 'fbcomments': fbcomments, 'twcount': twcount }
-    return '''<p class="sharecounts" data-for="%(url)s">
-    <span class="facebook-count"    data-n="%(fbcount)d">%(fbcount)d fb total.</span>    
-    <span class="facebook-shares"   data-n="%(fbshares)d">%(fbshares)d likes.</span>    
-    <span class="facebook-comments" data-n="%(fbcomments)d">%(fbcomments)d comments.</span>    
-    <span class="twitter-count"     data-n="%(twcount)d">%(twcount)d tweets.</span>    
-</p>''' % data
-
+    return render_template('sharecounts_ssi.html', **data)
 
